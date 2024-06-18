59220a9938b99a58f6c6fa133bde388e9c1ffb96
Stofi09 <kristof.solak@gmail.com>
2024-06-18 20:57:47 +0200

    FrHandler added

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/Handlers/FrVatRegistrationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Taxually.TechnicalTest.Models;
+using Taxually.TechnicalTest.Services.Interfaces;
+
+namespace Taxually.TechnicalTest.Services.Handlers
+{
+    public class FrVatRegistrationHandler : IVatRegistrationHandler
+    {
+        private readonly ITaxuallyQueueClient _queueClient;
+
+        public FrVatRegistrationHandler(ITaxuallyQueueClient queueClient)
+        {
+            _queueClient = queueClient;
+        }
+
+        //Add try catch and config
+        public async Task HandleAsync(VatRegistrationRequest request)
+        {
+            var csvBuilder = new StringBuilder();
+            csvBuilder.AppendLine("CompanyName,CompanyId");
+            csvBuilder.AppendLine($"{request.CompanyName},{request.CompanyId}");
+            var csv = Encoding.UTF8.GetBytes(csvBuilder.ToString());
+            await _queueClient.EnqueueAsync("vat-registration-csv", csv);
+        }
+    }
+}
