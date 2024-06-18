15873bad58a0e4b9782a38b6181d145d57e016e3
Stofi09 <kristof.solak@gmail.com>
2024-06-18 20:53:45 +0200

    Reg.Service added

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/VatRegistrationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taxually.TechnicalTest.Models;
+using Taxually.TechnicalTest.Services.Interfaces;
+
+namespace Taxually.TechnicalTest.Services
+{
+    public class VatRegistrationService
+    {
+        private readonly Dictionary<string, IVatRegistrationHandler> _handlers;
+        public VatRegistrationService(IEnumerable<IVatRegistrationHandler> handlers)
+        {
+            _handlers = handlers.ToDictionary(h => h.GetType().Name.Replace("VatRegistrationHandler", ""), h => h);
+        }
+
+        public async Task<ActionResult> RegisterVatAsync(VatRegistrationRequest request)
+        {
+            if (_handlers.TryGetValue(request.Country, out var handler))
+            {
+                await handler.HandleAsync(request);
+                return new OkResult();
+            }
+
+            return new BadRequestObjectResult("Country not supported");
+        }
+    }
+}
