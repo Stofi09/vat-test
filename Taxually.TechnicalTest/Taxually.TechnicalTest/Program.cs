74943d0fd421c045a47aaf013b29063b25db3315
Stofi09 <kristof.solak@gmail.com>
2024-06-18 21:05:18 +0200

    Services registered

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
@@ -1,8 +1,18 @@
+using Taxually.TechnicalTest.Services.Handlers;
+using Taxually.TechnicalTest.Services.Interfaces;
+using Taxually.TechnicalTest.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<ITaxuallyHttpClient, TaxuallyHttpClient>();
+builder.Services.AddTransient<ITaxuallyQueueClient, TaxuallyQueueClient>();
+builder.Services.AddTransient<IVatRegistrationHandler, UkVatRegistrationHandler>();
+builder.Services.AddTransient<IVatRegistrationHandler, FrVatRegistrationHandler>();
+builder.Services.AddTransient<IVatRegistrationHandler, DeVatRegistrationHandler>();
+builder.Services.AddTransient<VatRegistrationService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
