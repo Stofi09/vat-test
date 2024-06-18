7ca2d6bdc3467f72dd87d1c74a28493165038377
Stofi09 <kristof.solak@gmail.com>
2024-06-18 20:43:12 +0200

    Model relocated

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Controllers/VatRegistrationController.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Controllers/VatRegistrationController.cs
@@ -52,11 +52,4 @@ namespace Taxually.TechnicalTest.Controllers
             return Ok();
         }
     }
-
-    public class VatRegistrationRequest
-    {
-        public string CompanyName { get; set; }
-        public string CompanyId { get; set; }
-        public string Country { get; set; }
-    }
 }
