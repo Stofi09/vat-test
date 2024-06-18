97cfefe0eb19f19e59116db47841ec71da173750
Stofi09 <kristof.solak@gmail.com>
2024-06-18 21:12:29 +0200

    Basic validation added

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Models/VatRegistrationRequest.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Models/VatRegistrationRequest.cs
@@ -1,9 +1,17 @@
-﻿namespace Taxually.TechnicalTest.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxually.TechnicalTest.Models
 {
     public class VatRegistrationRequest
     {
+        [Required(ErrorMessage = "CompanyName is required.")]
         public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "CompanyId is required.")]
         public string CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
+        [StringLength(2, ErrorMessage = "Country code must be 2 characters.")]
         public string Country { get; set; }
     }
 }
