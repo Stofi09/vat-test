587c5cb22444026fe32b153e2d963b7c8215145b
Stofi09 <kristof.solak@gmail.com>
2024-06-18 20:56:12 +0200

    UkHandler added

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/Handlers/UkVatRegistrationHandler.cs
@@ -0,0 +1,21 @@
+﻿using Taxually.TechnicalTest.Models;
+using Taxually.TechnicalTest.Services.Interfaces;
+
+namespace Taxually.TechnicalTest.Services.Handlers
+{
+    public class UkVatRegistrationHandler : IVatRegistrationHandler
+    {
+        private readonly ITaxuallyHttpClient _httpClient;
+
+        public UkVatRegistrationHandler(ITaxuallyHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        //Add try catch, get the link from config
+        public async Task HandleAsync(VatRegistrationRequest request)
+        {
+            await _httpClient.PostAsync("https://api.uktax.gov.uk", request);
+        }
+    }
+}
